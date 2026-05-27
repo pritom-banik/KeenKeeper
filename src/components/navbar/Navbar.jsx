d5f315ea1981038ca636ec84731ee4d38db47407
UnpolishedNoob <banikpritom321@gmail.com>
2026-05-28 02:24:09 +0600

    designed navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,30 @@
+import { TiHomeOutline } from "react-icons/ti";
+import { FiClock } from "react-icons/fi";
+import { BiStats } from "react-icons/bi";
+import { NavLink } from "react-router";
+
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+      <div className="flex-1 px-10">
+        <div className="text-2xl font-black">Keen<span className="text-[#244d3fFF]">Keeper</span></div>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+        <ul className="menu menu-horizontal px-1 font-bold ">
           <li>
-            <a>Link</a>
+            <NavLink to="/">
+            <div className="flex justify-center items-center gap-0.5 p-1"><TiHomeOutline /><h6>Home</h6></div>
+            </NavLink>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <NavLink to="/timeline">
+            <div className="flex justify-center items-center gap-0.5 p-1"><FiClock /><h6>Timeline</h6></div> 
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/stats">
+            <div className="flex justify-center items-center gap-0.5 p-1"><BiStats /><h6>Stats</h6></div>
+            </NavLink>
           </li>
         </ul>
       </div>
