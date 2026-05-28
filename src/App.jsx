8f15c98cffc9a65c0074bc3e9202c246879e21e8
UnpolishedNoob <banikpritom321@gmail.com>
2026-05-28 15:58:41 +0600

    Added footer to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import Navbar from "./components/navbar/Navbar"
-
+import { Outlet } from "react-router";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  
-
   return (
-    <>
-      
-<Navbar></Navbar>
-     
-    </>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Navbar></Navbar>
+      <div className="grow">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,23 +7,34 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1 px-10">
-        <div className="text-2xl font-black">Keen<span className="text-[#244d3fFF]">Keeper</span></div>
+        <div className="text-2xl font-black">
+          Keen<span className="text-[#244d3fFF]">Keeper</span>
+        </div>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1 font-bold ">
           <li>
             <NavLink to="/">
-            <div className="flex justify-center items-center gap-0.5 p-1"><TiHomeOutline /><h6>Home</h6></div>
+              <div className="flex justify-center items-center gap-0.5 p-1">
+                <TiHomeOutline />
+                <h6>Home</h6>
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink to="/timeline">
-            <div className="flex justify-center items-center gap-0.5 p-1"><FiClock /><h6>Timeline</h6></div> 
+              <div className="flex justify-center items-center gap-0.5 p-1">
+                <FiClock />
+                <h6>Timeline</h6>
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink to="/stats">
-            <div className="flex justify-center items-center gap-0.5 p-1"><BiStats /><h6>Stats</h6></div>
+              <div className="flex justify-center items-center gap-0.5 p-1">
+                <BiStats />
+                <h6>Stats</h6>
+              </div>
             </NavLink>
           </li>
         </ul>
