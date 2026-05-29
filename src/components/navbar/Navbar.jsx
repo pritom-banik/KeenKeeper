3817353958ca2abbe250aa9c8a13f367057cf957
UnpolishedNoob <banikpritom321@gmail.com>
2026-05-29 20:41:57 +0600

    Designed navbar for small screen also

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,39 +1,84 @@
 import { TiHomeOutline } from "react-icons/ti";
-import { FiClock } from "react-icons/fi";
+import { FiClock, FiMenu } from "react-icons/fi";
 import { BiStats } from "react-icons/bi";
 import { NavLink } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1 px-10">
-        <div className="text-2xl font-black">
+    <div className="navbar bg-base-100 shadow-sm px-3 sm:px-6 md:px-10">
+      
+      <div className="flex-1">
+        <div className="text-xl sm:text-2xl font-black">
           Keen<span className="text-[#244d3fFF]">Keeper</span>
         </div>
       </div>
-      <div className="flex-none">
+
+      
+      <div className="hidden md:flex">
         <ul className="menu menu-horizontal px-1 font-bold gap-1">
           <li>
             <NavLink to="/">
-              <div className="flex justify-center items-center gap-0.5 p-1">
+              <div className="flex items-center gap-1 p-1">
                 <TiHomeOutline />
                 <h6>Home</h6>
               </div>
             </NavLink>
           </li>
+
           <li>
             <NavLink to="/timeline">
-              <div className="flex justify-center items-center gap-0.5 p-1">
+              <div className="flex items-center gap-1 p-1">
                 <FiClock />
                 <h6>Timeline</h6>
               </div>
             </NavLink>
           </li>
+
           <li>
             <NavLink to="/stats">
-              <div className="flex justify-center items-center gap-0.5 p-1">
+              <div className="flex items-center gap-1 p-1">
                 <BiStats />
                 <h6>Stats</h6>
+              </div>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      {/* Mobile Dropdown */}
+      <div className="dropdown dropdown-end md:hidden">
+        <label tabIndex={0} className="btn btn-ghost">
+          <FiMenu size={22} />
+        </label>
+
+        <ul
+          tabIndex={0}
+          className=" menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-bold
+          "
+        >
+          <li>
+            <NavLink to="/">
+              <div className="flex items-center gap-2">
+                <TiHomeOutline />
+                Home
+              </div>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/timeline">
+              <div className="flex items-center gap-2">
+                <FiClock />
+                Timeline
+              </div>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/stats">
+              <div className="flex items-center gap-2">
+                <BiStats />
+                Stats
               </div>
             </NavLink>
           </li>
